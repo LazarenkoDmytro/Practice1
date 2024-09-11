01374e334ff7ac1541ce59cf79c25c9efd4a9545
LazarenkoDmytro <lazarenkodmytro.work@gmail.com>
2024-09-12 01:09:34 +0300

    Third task

--- src/main/practice/first/Main.java
+++ src/main/practice/first/Main.java
@@ -1,8 +1,11 @@
 package main.practice.first;
 
+import java.util.Scanner;
+
 public class Main {
     private static void splitFiveNumber(int num) {
-        System.out.printf("%d%n%d%n%d%n%d%n%d%n", num / 10_000, num / 1_000 % 10, num / 100 % 10, num / 10 % 10, num % 10);
+        System.out.printf("%d%n%d%n%d%n%d%n%d%n", num / 10_000, num / 1_000 % 10, num / 100 % 10, num / 10 % 10,
+                num % 10);
     }
 
     private static void triangleSquareBySides(double[] sides) {
@@ -12,11 +15,24 @@ public class Main {
         System.out.printf("%.2f%n", square);
     }
 
+    private static void circleLengthByRadius() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of circle: ");
+        double radius = scanner.nextDouble();
+        double circleLength = 2 * Math.PI * radius;
+
+        System.out.printf("%.2f%n", circleLength);
+    }
+
     public static void main(String[] args) {
         splitFiveNumber(54689);
 
         System.out.println();
 
         triangleSquareBySides(new double[]{3, 4, 5});
+
+        System.out.println();
+
+        circleLengthByRadius();
     }
 }
